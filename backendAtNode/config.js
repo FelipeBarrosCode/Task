56b2385311df1c9474a528aebf5c9f4c5dc1155d
FelipeBarrosCode <0801.felipe@gmail.com>
2025-06-15 13:56:38 -0700

    Added changes

--- backendAtNode/config.js
+++ backendAtNode/config.js
@@ -8,7 +8,7 @@ module.exports = {
   mongodbUri: process.env.MONGODB_URI || 'mongodb+srv://0801felipe:Casa8301@cluster0.mcf6cuy.mongodb.net/logs_db?retryWrites=true&w=majority&appName=Cluster0',
   
   // JWT
-  jwtSecret: process.env.JWT_SECRET || '6acf2744282fae154fba99d84b1e5e6029634e9cea75c37bd8993358564c98d76824c95ed842c8e9d94dadeb039a1e599720b08e42b379df0227e8e50450d95a',
+  jwtSecret: process.env.JWT_SECRET || '8ab2f76d544d6b04071ea40e3c6c3c02fb18e90651950787d99eb1df0feae1cbc9fccb8efc9d6b52236536bc9be04a96a4f4cdbdfb4925f6b6211f7ed14b0654',
   jwtExpireMinutes: parseInt(process.env.JWT_EXPIRE_MINUTES) || 60,
   
   // CORS
